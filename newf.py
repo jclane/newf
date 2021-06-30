7bc8e7513b215e1acd0c4434c9ca93d968434a80
jclane <justin@justinlane.me>
2021-06-30 21:14:27 +0000

    working on reading dir and storing comparing state

--- newf.py
+++ newf.py
@@ -3,29 +3,42 @@
 import sys
 from csv import writer, reader
 from os import walk as oswalk
+from os import getcwd as osgetcwd
 from os.path import join as path_join
 
-ARGS = {arg[0]: arg[1] for arg in enumerate(sys.argv[1:])}
+#ARGS = {arg[0]: arg[1] for arg in enumerate(sys.argv[1:])}
 
 # following needs a command line argument or it will error
-dirs = next(oswalk(ARGS[0]))[1]
+#dirs = next(oswalk(ARGS[0]))[1]
 
-def csvreader():
-    data = []
-    with open(path_join(ARGS[0], r".dir_list.csv"), "r", newline="\n") as f:
-        lines = reader(f, delimiter=",")
-        for line in lines:
-            data.append(line)
+class DirectoryListHandler:
 
-    return data
+    dir_list = []
 
-def csvwriter(data):
-    with open(path_join(ARGS[0], r".dir_list.csv"), "w+", newline="\n") as f:
+    def __init__(self, path=osgetcwd()):
+        self.path = path
+        self.dirs = next(oswalk(path))[1]
+        self.dir_list = self.csvreader()
+
+    def get_mtime(self, file):
+        print(file)
+
+    def csvreader(self):
+        data = []
+        with open(path_join(self.path, r".dir_list.csv"), "w+", newline="\n") as f:
+            lines = reader(f, delimiter=",")
+            for line in lines:
+                print(os.path.getmtime(line))
+                data.append({"name":line})
+
+        return data
+
+def csvwriter(path):
+    with open(path_join(path, r".dir_list.csv"), "w+", newline="\n") as f:
         w = writer(f, delimiter=",")
         for line in data:
-            print(line)
             w.writerow(line)
 
-csvwriter([["docs"], ["pics"], ["books"], ["bin"]])
-data = csvreader()
-#print(data)
+
+dl = DirectoryListHandler()
+
